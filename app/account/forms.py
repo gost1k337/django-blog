f5244b97f82ea5d18e16941b698e3faa6f3de279
gost3228 <gost1k337@gmail.com>
2020-10-22 08:56:23 +0300

    add register and login forms

--- app/account/forms.py
+++ app/account/forms.py
@@ -3,7 +3,28 @@ from .models import Account
 
 
 class RegisterForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput
+        (attrs={'class': 'text', 'name': 'Username', 'placeholder': 'Username'}))
+
+    email = forms.EmailField(widget=forms.EmailInput
+        (attrs={'class': 'text email', 'name': 'email', 'placeholder': 'Email'}))
+
+    password = forms.CharField(widget=forms.PasswordInput
+        (attrs={'class': 'text password', 'name': 'password', 'placeholder': 'Password'}))
+
     class Meta:
         model = Account
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
+
+
+class LoginForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailInput
+        (attrs={'class': 'text email', 'name': 'email', 'placeholder': 'Email'}))
+
+    password = forms.CharField(widget=forms.PasswordInput
+        (attrs={'class': 'text password', 'name': 'password', 'placeholder': 'Password'}))
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email', 'password')
 
--- app/account/views.py
+++ app/account/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from .models import Account
 from django.views.generic import View, TemplateView
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 class RegisterView(View):
+
     def get(self, request):
         form = RegisterForm()
         return render(request, 'account/register.html', {'form': form})
 
 
-class LoginView(TemplateView):
-    template_name = 'account/login.html'
+class LoginView(View):
+
+    def get(self, request):
+        form = LoginView()
+        return render(request, 'account/login.html', {'form': form})
