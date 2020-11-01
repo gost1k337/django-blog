c765c9c963accf8ca5a0b5b7462f24e6eaed1d27
gost3228 <gost1k337@gmail.com>
2020-11-01 15:58:48 +0300

    change templates

--- app/account/admin.py
+++ app/account/admin.py
@@ -10,7 +10,7 @@ class UserAdmin(DjangoUserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('username', 'first_name', 'second_name')}),
+        (_('Personal info'), {'fields': ('username', 'first_name', 'second_name', 'followers')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
--- app/blog_app/views.py
+++ app/blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
 
 class HomeView(LoginRequiredMixin, View):
@@ -14,4 +15,9 @@ class ProfileView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
 
     def get(self, request):
-        return render(request, 'blog_app/profile_page.html')
+        # User = get_user_model()
+        context = {}
+        user = request.user
+        context['user'] = user
+        context['followers'] = user.followers.all()[:5]
+        return render(request, 'blog_app/profile_page.html', context=context)
